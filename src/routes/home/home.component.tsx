f1181ef488b5f569bd73c296e6eb10fda9bd9ec6
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-06-04 21:09:59 +0200

    initial commit

---
+++ src/routes/home/home.component.tsx
@@ -0,0 +1,21 @@
+const Home = () => {
+    return (
+        <div>
+            <form action="">
+                <label>Meter ID</label>
+                <input type="text" />
+                <label>Measurement</label>
+                <input type="text" />
+                <label>Start Date</label>
+                <input type="text" />
+                <label>Stop Date</label>
+                <input type="text" />
+                <label>Limit</label>
+                <input type="text" />
+                <button>View</button>
+            </form>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/routes/navigation/navigation.component.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom"
+
+const Navigation = () => {
+    return (
+        <>
+        <div>
+            nav
+        </div>
+        <Outlet/>
+        </>
+        
+    )
+}
+
+export default Navigation
