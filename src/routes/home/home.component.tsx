397eaa3db7266e8ea4b64286bef6dbaf354a79a0
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-06-10 17:52:19 +0100

    add date picker

--- src/routes/home/home.component.tsx
+++ src/routes/home/home.component.tsx
@@ -1,22 +1,24 @@
-import { Box, Button, Paper, TextField } from "@mui/material";
+import { Box, Button, Paper, TextField, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import { ChangeEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import FormInput from "../../components/form-input/form-input.component";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+
 import "./home.styles.css";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 
 interface FormFields {
   meterID: string;
   measurement: string;
-  start: string;
-  stop: string;
+  start: Date | null;
+  stop: Date | null;
 }
 
 const defaultFormFields = {
   meterID: "53d63d3d-1b29-49a6-8e5f-537c730f4d11",
   measurement: "energy",
-  start: "2022-04-01",
-  stop: "2022-05-01",
+  start: new Date(),
+  stop: new Date(),
 };
 
 const Home = () => {
@@ -28,11 +30,19 @@ const Home = () => {
     setFormFields({ ...formFields, [name]: value });
   };
 
+  const handleDateChange = (e: { name: string; value: Date }) => {
+    const { name, value } = e;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
   return (
     // <div className="authentication-container">
     <Container maxWidth="sm">
       <Paper>
         <div style={{ padding: "40px" }}>
+          <Typography variant="h5" component="h1" gutterBottom>
+            Smart Meter Data Selection
+          </Typography>
           <TextField
             id="outlined-basic"
             label="Meter ID"
@@ -54,6 +64,30 @@ const Home = () => {
             disabled
             margin="dense"
           />
+          <Box marginTop={2} display="flex" justifyContent={"space-between"}>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker
+                label="Start Date"
+                value={formFields.start}
+                onChange={(newValue) => {
+                  handleDateChange({ name: "start", value: newValue as Date });
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker
+                label="Stop Date"
+                value={formFields.stop}
+                minDate={formFields.start}
+                onChange={(newValue) => {
+                  handleDateChange({ name: "stop", value: newValue as Date });
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+          </Box>
+
           <Box marginTop={2}>
             <Button
               variant="contained"
