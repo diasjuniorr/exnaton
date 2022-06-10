23090f473c7a0e6b4af502865c79f8ade7707821
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-06-10 13:43:56 +0100

    add material ui

--- src/routes/home/home.component.tsx
+++ src/routes/home/home.component.tsx
@@ -1,3 +1,5 @@
+import { Box, Button, Paper, TextField } from "@mui/material";
+import { Container } from "@mui/system";
 import { ChangeEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import FormInput from "../../components/form-input/form-input.component";
@@ -14,12 +16,12 @@ const defaultFormFields = {
   meterID: "53d63d3d-1b29-49a6-8e5f-537c730f4d11",
   measurement: "energy",
   start: "2022-04-01",
-  stop: "2022-05-01"
+  stop: "2022-05-01",
 };
 
 const Home = () => {
   const [formFields, setFormFields] = useState<FormFields>(defaultFormFields);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -27,31 +29,45 @@ const Home = () => {
   };
 
   return (
-    <div className="authentication-container">
-      <form action="" style={{width: "100%"}}>
-        <FormInput
-          label="Meter ID"
-          type="text"
-          name="meterID"
-          value={formFields.meterID}
-          onChange={handleChange}
-          disabled
-        />
-        <FormInput
-          label="Measurement"
-          type="text"
-          name="measurement"
-          value={formFields.measurement}
-          onChange={handleChange}
-          disabled
-        />
-        {/* <label>Start Date</label> */}
-        {/* <input type="text" /> */}
-        {/* <label>Stop Date</label> */}
-        {/* <input type="text" /> */}
-        <button onClick={()=> navigate("/line-chart", {state: formFields})}>View</button>
-      </form>
-    </div>
+    // <div className="authentication-container">
+    <Container maxWidth="sm">
+      <Paper>
+        <div style={{ padding: "40px" }}>
+          <TextField
+            id="outlined-basic"
+            label="Meter ID"
+            name="meterID"
+            value={formFields.meterID}
+            variant="standard"
+            fullWidth
+            onChange={handleChange}
+            disabled
+            margin="dense"
+          />
+          <TextField
+            label="Measurement"
+            variant="standard"
+            name="measurement"
+            value={formFields.measurement}
+            fullWidth
+            onChange={handleChange}
+            disabled
+            margin="dense"
+          />
+          <Box marginTop={2}>
+            <Button
+              variant="contained"
+              fullWidth
+              onClick={() => navigate("/line-chart", { state: formFields })}
+            >
+              View
+            </Button>
+          </Box>
+        </div>
+      </Paper>
+    </Container>
+
+    // </div>
   );
 };
 
