5002ad6d40371e88e5c040dedc87232257950cac
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-06-13 20:29:39 +0100

    add navigation bar

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -1,15 +1,28 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom";
+
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 const Navigation = () => {
-    return (
-        <>
-        <div>
-            nav
-        </div>
-        <Outlet/>
-        </>
-        
-    )
-}
+  const navigate = useNavigate();
 
-export default Navigation
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <span onClick={() => navigate("/")} style={{ cursor: "pointer" }}>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              EXNATON
+            </Typography>
+          </span>
+        </Toolbar>
+      </AppBar>
+      <Outlet />
+    </Box>
+  );
+};
+
+export default Navigation;
