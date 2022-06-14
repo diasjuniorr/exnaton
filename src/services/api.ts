5c78d500985ab5445dc9e8d297da81423e1d7282
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-06-14 22:24:33 +0100

    add api service

---
+++ src/services/api.ts
@@ -0,0 +1,26 @@
+interface ApiResponse {
+  date: string;
+  measurement: string;
+  aETotalImport: number;
+  aETotalExport: number;
+  measurements: Measurement[];
+}
+
+interface Measurement {
+  "0100011D00F": number;
+  "0100021D01F": number;
+  time: string;
+}
+
+function api<ApiResponse>(endpoint: string): Promise<ApiResponse> {
+  return fetch(`https://exnaton-backend.herokuapp.com${endpoint}`).then(
+    (response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    }
+  );
+}
+
+export default api;
