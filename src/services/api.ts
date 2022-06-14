b554b729cc5730dba9b5af6dd8cc4cb9cc73abd7
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-06-15 00:16:36 +0100

    refact api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,26 +1,9 @@
-interface ApiResponse {
-  date: string;
-  measurement: string;
-  aETotalImport: number;
-  aETotalExport: number;
-  measurements: Measurement[];
-}
-
-interface Measurement {
-  "0100011D00F": number;
-  "0100021D01F": number;
-  time: string;
-}
-
-function api<ApiResponse>(endpoint: string): Promise<ApiResponse> {
-  return fetch(`https://exnaton-backend.herokuapp.com/api/v1${endpoint}`).then(
-    (response) => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
-    }
+export async function api<T>(endpoint: RequestInfo): Promise<T> {
+  const response = await fetch(
+    `https://exnaton-backend.herokuapp.com/api/v1${endpoint}`
   );
+  const body = await response.json();
+  return body;
 }
 
 export default api;
