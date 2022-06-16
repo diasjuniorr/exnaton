d17934be53d06581e8fe3ed24b3cbdda4fa37d2c
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-06-16 10:09:01 +0100

    add try catch to api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,17 @@
 export async function api<T>(endpoint: RequestInfo): Promise<T> {
-  const response = await fetch(
-    `https://exnaton-backend.herokuapp.com/api/v1${endpoint}`
-  );
-  const body = await response.json();
-  return body;
+  try {
+    const response = await fetch(
+      `https://exnaton-backend.herokuapp.com/api/v1${endpoint}`
+    );
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    const body = await response.json();
+    return body;
+  } catch (error) {
+    throw new Error("Request failed: " + error);
+  }
 }
 
 export default api;
