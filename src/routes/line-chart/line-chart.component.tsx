674ab76d8acda7b08e7f9c36602eacbcdd1e6fa8
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-06-17 11:00:31 +0100

    remove unused imports

--- src/routes/line-chart/line-chart.component.tsx
+++ src/routes/line-chart/line-chart.component.tsx
@@ -1,4 +1,4 @@
-import { Paper, TextField, Typography } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
